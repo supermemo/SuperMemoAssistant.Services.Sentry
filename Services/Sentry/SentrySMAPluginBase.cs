5af48b075d5a347dc3dbe4505cc474d5ced8da51
Alexis <alexis@incogito.org>
2020-04-25 03:56:59 +0200

    - Updated: New interop - Updated: Applied coding style

--- Services/Sentry/SentrySMAPluginBase.cs
+++ Services/Sentry/SentrySMAPluginBase.cs
@@ -6,7 +6,7 @@
 // copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the 
+// and/or sell copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following conditions:
 // 
 // The above copyright notice and this permission notice shall be included in
@@ -19,26 +19,22 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
-// 
-// 
-// Created On:   2019/02/23 16:23
-// Modified On:  2019/02/23 16:55
-// Modified By:  Alexis
 
 #endregion
 
 
 
 
-using System;
-using Anotar.Serilog;
-using Serilog;
-using SuperMemoAssistant.Extensions;
-using SuperMemoAssistant.Interop.Plugins;
-
 // ReSharper disable once CheckNamespace
 namespace SuperMemoAssistant.Services.Sentry
 {
+  using System;
+  using Anotar.Serilog;
+  using Extensions;
+  using Interop.Plugins;
+  using Serilog;
+
+  /// <inheritdoc />
   public abstract class SentrySMAPluginBase<TPlugin> : SMAPluginBase<TPlugin>
     where TPlugin : SentrySMAPluginBase<TPlugin>
   {
@@ -54,10 +50,7 @@ namespace SuperMemoAssistant.Services.Sentry
     #region Constructors
 
     /// <inheritdoc />
-    protected SentrySMAPluginBase(
-      string                 sentryId,
-      DebuggerAttachStrategy debuggerAttachStrategy = DebuggerAttachStrategy.Never)
-      : base(debuggerAttachStrategy)
+    protected SentrySMAPluginBase(string sentryId)
     {
       var pluginType = typeof(TPlugin);
       // ReSharper disable once VirtualMemberCallInConstructor
@@ -66,7 +59,8 @@ namespace SuperMemoAssistant.Services.Sentry
       _sentry = SentryEx.Initialize(sentryId, releaseName);
     }
 
-    public override void Dispose()
+    /// <inheritdoc />
+    protected override void Dispose(bool disposing)
     {
       try
       {
@@ -77,7 +71,7 @@ namespace SuperMemoAssistant.Services.Sentry
         LogTo.Error(ex, "Exception while disposing Sentry instance");
       }
 
-      base.Dispose();
+      base.Dispose(disposing);
     }
 
     #endregion
@@ -87,6 +81,7 @@ namespace SuperMemoAssistant.Services.Sentry
 
     #region Methods Impl
 
+    /// <inheritdoc />
     protected override LoggerConfiguration ConfigureLogger(LoggerConfiguration loggerConfiguration)
     {
       return loggerConfiguration.LogToSentry();
